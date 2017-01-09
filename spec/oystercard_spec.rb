955b8d547710d014cd74b8bf609059215e378cb2
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-09 18:29:01 +0000

    5 Top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,11 +2,17 @@ require 'oystercard'
 
 describe Oystercard do
 
-  #it {is_expected.to respond_to(:balance)}
+  it {is_expected.to respond_to(:top_up).with(1).argument}
 
   it 'creates a freshly initialized card with balance 0' do
-    oyster = Oystercard.new
-    expect(oyster.balance).to eq(0)
+    card = Oystercard.new
+    expect(card.balance).to eq(0)
+  end
+
+  it 'tests topup' do
+    card = Oystercard.new
+    value_topup = 5
+    expect(card.top_up(value_topup)).to eq(card.balance)
   end
 
 end
