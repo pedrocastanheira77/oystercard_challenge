87b65f0bc0da96d265a9a6359ddbf904a2a75cb2
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-09 18:11:26 +0000

    4 Add the balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,11 @@
 class Oystercard
+
+  INITIAL_BALANCE = 0
+
+  attr_reader :balance
+
+  def initialize(balance = INITIAL_BALANCE)
+    @balance = balance
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  #it {is_expected.to respond_to(:balance)}
+
+  it 'creates a freshly initialized card with balance 0' do
+    oyster = Oystercard.new
+    expect(oyster.balance).to eq(0)
+  end
+
 end
